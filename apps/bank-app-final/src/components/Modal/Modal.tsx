7699c8f46c75f120014e9659c41b202e2711193c
Mateusz Jabłoński <mail@mateuszjablonski.com>
2023-02-28 15:52:13 +0100

    modal + presentation

--- apps/bank-app-final/src/components/Modal/Modal.tsx
+++ apps/bank-app-final/src/components/Modal/Modal.tsx
@@ -1,16 +1,26 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { ModalBox, ModalWrapper } from './Modal.styled';
 
+const TIME_TO_CLOSE_MODAL = 4000;
+
 const Modal: FC<{ value: string }> = ({ value }) => {
-  return createPortal(
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsVisible(false);
+    }, TIME_TO_CLOSE_MODAL)
+  }, []);
+
+  return isVisible ? createPortal(
     <ModalWrapper>
       <ModalBox>
         test: {value}
       </ModalBox>
     </ModalWrapper>,
     document.getElementById('modal') as HTMLDivElement,
-  )
+  ) : null;
 }
 
 export default Modal;
